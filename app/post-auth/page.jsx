fd9dd2b34efd29094451b3a7cba180825050da96
paurasm22 <paurasmore22@gmail.com>
2025-09-15 08:25:49 +0530

    My local changes before merging upstream

--- app/post-auth/page.jsx
+++ app/post-auth/page.jsx
@@ -4,7 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 import connectToDB from "@/lib/mongoose";
 import User from "@/models/User";
 
-const ADMIN_EMAIL = "shraddhaghuleshraddha@example.com";
+const ADMIN_EMAIL = "paurasmore22@gmail.com";
 
 export default async function PostAuthPage() {
   const { userId } = auth(); // server-side auth
